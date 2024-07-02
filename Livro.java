a31fe8998322ca81454c1c359a2d7b542e032103
rhaiisr <rhaissareis039@gmail.com>
2024-07-02 11:50:52 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    String nomeL;
+
+    public Livro(String nomeLivro) {
+        nomeL = nomeLivro;
+    }
+
+    public void detalhes() {
+        System.out.println("Nome do livro: " + livro);
+    }
+}
