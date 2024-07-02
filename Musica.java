88583f46e35cbafc6142fa6dbbe7adc97c98f1ee
rhaiisr <rhaissareis039@gmail.com>
2024-07-02 11:47:42 -0300

    Create Musica.java

---
+++ Musica.java
@@ -0,0 +1,11 @@
+public class Musica{
+    String nomeM;
+
+    public Musica(String nomeMusica) {
+        nomeM = nomeMusica;
+    }
+
+    public void detalhes() {
+        System.out.println("O nome da música é: " + nomeM);
+    }
+}
