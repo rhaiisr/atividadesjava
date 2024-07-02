404b960555a6257be2adb7b84001338966f6096b
rhaiisr <rhaissareis039@gmail.com>
2024-07-02 11:50:21 -0300

    Create Bicicleta.java

---
+++ Bicicleta.java
@@ -0,0 +1,11 @@
+public class Bicicleta {
+    String modelo;
+
+    public Livro(String modeloBicicleta) {
+        modelo = modeloBicicleta;
+    }
+
+    public void detalhes() {
+        System.out.println("Modelo da Bicicleta: " + modelo);
+    }
+}
